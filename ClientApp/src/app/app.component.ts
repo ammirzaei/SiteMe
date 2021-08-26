40f1680e1dbb8cea2a6ffe1b91681547e0931df0
ammirzaei <ammirzaei.dev@gmail.com>
2021-08-26 23:28:15 +0430

    Update loader

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   constructor() {
-
-  }
-  ngAfterViewInit(): void {
-    document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('load', () => {
       document.getElementsByClassName('loader')[0].classList.add('hide');
     });
   }
