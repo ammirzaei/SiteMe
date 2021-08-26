d608e63dbdc8a8ccb65af0b4d52659a2a5448288
ammirzaei <ammirzaei.dev@gmail.com>
2021-08-26 23:16:01 +0430

    Added loader first document

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SiteMe';
-  
+export class AppComponent implements AfterViewInit {
+  constructor() {
+
+  }
+  ngAfterViewInit(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      document.getElementsByClassName('loader')[0].classList.add('hide');
+    });
+  }
 }
