ac2671474d3704aec8970c05efa296f7b9aecaaf
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-03 17:11:43 +0430

    Migration for update contact

---
+++ ServerApp/SiteMe/Migrations/20210901095219_mig-UpdateContactforIP.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SiteMe.Migrations
+{
+    public partial class migUpdateContactforIP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IP",
+                table: "Contact",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IP",
+                table: "Contact");
+        }
+    }
+}
