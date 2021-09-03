9f4477353c8aae96815d4e2f3881348242b51671
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-03 17:12:15 +0430

    Added classes for send email

---
+++ ServerApp/SiteMe/Models/Classes/SendEmail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using SiteMe.Models.Entities;
+
+namespace SiteMe.Models.Classes
+{
+    public static class SendEmail
+    {
+        public static void Send(string To, string Subject, string Body, Setting setting)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient client = new SmtpClient();
+                message.From = new MailAddress(setting.Email, "سایت شخصی امیرمحمد میرزائی");
+                message.To.Add(To);
+                message.Body = Body;
+                message.Subject = Subject;
+                message.IsBodyHtml = true;
+
+                client.Host = "mail.ammirzaei.ir";
+                client.Port = 25;
+                client.EnableSsl = false;
+                client.Credentials = new NetworkCredential(setting.Email, setting.EmailPassword);
+
+                client.Send(message);
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
