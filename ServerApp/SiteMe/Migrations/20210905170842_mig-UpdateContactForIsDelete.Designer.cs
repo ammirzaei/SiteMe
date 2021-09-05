2f49989596d7261c2feb28057403ae8a5b231e99
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-05 21:53:09 +0430

    Added Migration for add isDelete in contact entity

--- ServerApp/SiteMe/Migrations/20210905170842_mig-UpdateContactForIsDelete.Designer.cs
+++ ServerApp/SiteMe/Migrations/20210905170842_mig-UpdateContactForIsDelete.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteMe.Models.Context;
 
 namespace SiteMe.Migrations
 {
     [DbContext(typeof(SiteMeContext))]
-    partial class SiteMeContextModelSnapshot : ModelSnapshot
+    [Migration("20210905170842_mig-UpdateContactForIsDelete")]
+    partial class migUpdateContactForIsDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
