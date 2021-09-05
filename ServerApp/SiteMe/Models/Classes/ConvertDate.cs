302be373d28969a9fee4bced9658d3b6364c7405
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-05 19:52:19 +0430

    Added ConvertDate for convert date with custom date

---
+++ ServerApp/SiteMe/Models/Classes/ConvertDate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteMe.Models.Classes
+{
+    public static class ConvertDate
+    {
+        public static string ToShamsi(this DateTime date)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetHour(date).ToString("00") + ":" + pc.GetMinute(date).ToString("00") + ":" +
+                   pc.GetSecond(date).ToString("00") + " "
+                   + pc.GetYear(date) + "/" + pc.GetMonth(date).ToString("00") + "/" +
+                   pc.GetDayOfMonth(date).ToString("00");
+        }
+    }
+}
