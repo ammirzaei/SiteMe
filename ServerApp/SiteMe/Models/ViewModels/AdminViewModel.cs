e29f6ca40c876fc3cae8d25682059f521f75b4f1
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-06 19:19:44 +0430

    Added AdminViewModel

---
+++ ServerApp/SiteMe/Models/ViewModels/AdminViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteMe.Models.ViewModels
+{
+    public class AdminViewModel
+    {
+        public string DateNow { get; set; }
+        public int CountMessage { get; set; }
+    }
+}
