aa96e946885790c7759b172ddc0d74e761317f93
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-06 19:20:12 +0430

    Update ConvertDate for added ToShamsiDate class

--- ServerApp/SiteMe/Models/Classes/ConvertDate.cs
+++ ServerApp/SiteMe/Models/Classes/ConvertDate.cs
@@ -16,5 +16,12 @@ namespace SiteMe.Models.Classes
                    + pc.GetYear(date) + "/" + pc.GetMonth(date).ToString("00") + "/" +
                    pc.GetDayOfMonth(date).ToString("00");
         }
+
+        public static string ToShamsiDate(this DateTime date)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(date) + "/" + pc.GetMonth(date).ToString("00") + "/" +
+                   pc.GetDayOfMonth(date).ToString("00");
+        }
     }
 }
