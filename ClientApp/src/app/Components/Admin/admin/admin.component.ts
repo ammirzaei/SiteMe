afe49ef8cfa1ecb9a17d2da9dbea34b2c9db48b0
ammirzaei <ammirzaei.dev@gmail.com>
2021-09-06 19:18:43 +0430

    Update Admin Component for logout and PanelInfo

--- ClientApp/src/app/Components/Admin/admin/admin.component.ts
+++ ClientApp/src/app/Components/Admin/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from './../../../Shared/Admin/admin.service';
+import { GetInfo } from './../../../Shared/Admin/Admin';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router, private _AdminService: AdminService) { }
 
   ngOnInit(): void {
+    this._AdminService.GetAdminPanelInfo().subscribe((success: any) => {
+      this.PanelInfo = success;
+    });
+    this._AdminService.ShareShowMessage.subscribe(res => {
+      this.PanelInfo.countMessage -= 1;
+    });
   }
-
+  PanelInfo: GetInfo = new GetInfo();
+  Logout() {
+    localStorage.removeItem('token');
+    this._router.navigate(["/"]);
+  }
 }
